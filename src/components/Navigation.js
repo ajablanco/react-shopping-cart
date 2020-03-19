d5cc27430fc851d385a4f388d1631fa31ab15216
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-03-19 11:49:32 -0700

    styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,20 @@
-import React, {useContext} from 'react';
-import { NavLink } from 'react-router-dom';
-import CartContext from '../contexts/CartContext';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import CartContext from "../contexts/CartContext";
 
 const Navigation = () => {
-	const cart = useContext(CartContext);
-	return (
-		<div className="navigation">
-			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{cart.length}</span>
-			</NavLink>
-		</div>
-	);
+  const cart = useContext(CartContext);
+  return (
+    <div className="navigation">
+      <h1 style={{ color: "white", fontSize: "2rem", marginLeft: "2%" }}>Aja's Favorite Books</h1>
+      <div>
+        <NavLink to="/">Products</NavLink>
+        <NavLink to="/cart">
+          Cart <span>{cart.length}</span>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
